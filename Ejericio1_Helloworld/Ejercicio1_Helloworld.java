f6517afa57bbf04e65517517abd1a729bd12ec2f
163721Aneliz <46726319+163721aneliz@users.noreply.github.com>
2019-02-05 13:27:15 -0700

    Add files via upload

---
+++ Ejericio1_Helloworld/Ejercicio1_Helloworld.java
@@ -0,0 +1,13 @@
+/**
+  * Nombre:Lizbeth Santillan 163721
+  * Fecha: 05/02/2019
+  * Hora de creación 12:22 p.m.
+*/
+
+class Ejercicio1_Helloworld {
+
+public static void main(String[] args) {
+  System.out.println("Hello\nAlan!");
+}
+
+}
