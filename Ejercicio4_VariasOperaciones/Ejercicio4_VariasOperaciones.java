870a9eb26d865bf39bef20cf1a0a940610942cd8
163721Aneliz <46726319+163721aneliz@users.noreply.github.com>
2019-02-12 13:47:12 -0700

    Add files via upload

---
+++ Ejercicio4_VariasOperaciones/Ejercicio4_VariasOperaciones.java
@@ -0,0 +1,19 @@
+/**
+  * Nombre: Lizbeth Santillan 163721
+  * Fecha: 12/02/2019
+  * Hora: 1:03 p.m. 
+*/
+
+class Ejercicio4_VariasOperaciones {
+
+public static void main(String[] args) {
+  
+  int a = (-5 + 8 * 6);
+  int b = ((55+9) % 9);
+  int c = (20 + (-3 * 5 / 8));
+  int d = (5 + 15 / 3 * 2 - 8 % 3);
+
+ System.out.println(a + "\n" + b + "\n" + c + "\n" + d); }
+}
+
+
