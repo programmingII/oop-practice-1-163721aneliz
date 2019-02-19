599b6fb0f3d81e9938fdadad1aea2df1f47212f3
163721Aneliz <46726319+163721aneliz@users.noreply.github.com>
2019-02-18 19:23:19 -0800

    Update Ejercicio8_Pattern.java

--- Ejercicio8_Pattern/Ejercicio8_Pattern.java
+++ Ejercicio8_Pattern/Ejercicio8_Pattern.java
@@ -8,10 +8,10 @@ class Ejercicio8 {
 
  public static void main(String[] args) {
 
- System.out.println("   J    a    V     V  a ");
- System.out.println("   J   a a    V   V  a a ");
- System.out.println("J  J  aaaaa    V V  aaaaa ");
- System.out.println(" JJ  a     a    V  a     a ");
+ System.out.println("   J    a    V     V   a ");
+ System.out.println("   J   a a    V   V   a a ");
+ System.out.println("J  J  aaaaa    V V   aaaaa ");
+ System.out.println(" JJ  a     a    V   a     a ");
 
 }
 }
