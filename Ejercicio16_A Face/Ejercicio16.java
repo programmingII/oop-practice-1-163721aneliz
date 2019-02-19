dbc34252033192e1eb80a71f1ac3e1ab84636432
163721Aneliz <46726319+163721aneliz@users.noreply.github.com>
2019-02-18 23:48:51 -0700

    Add files via upload

---
+++ Ejercicio16_A Face/Ejercicio16.java
@@ -0,0 +1,18 @@
+/**
+  * Nombre: Lizbeth Santillan 163721
+  * Fecha: 18/02/2019
+  * Hora: 11:45 p.m.
+*/
+
+class Ejercicio16 {
+
+public static void main(String[] args) {
+
+ System.out.println(" +\"\"\"\"\"+ ");
+ System.out.println("[| o o |]");
+ System.out.println(" |  ^  | ");
+ System.out.println(" | '-' | ");
+ System.out.println(" +-----+ ");
+ 
+}
+}
