9cd7d7749d39400ef0416775a499c8f9f552e7ed
163721Aneliz <46726319+163721aneliz@users.noreply.github.com>
2019-02-18 19:04:04 -0700

    Add files via upload

---
+++ Ejercicio5_TheProductOfTwoNumbers/Ejercicio5_TheProductOfTwoNumbers.java
@@ -0,0 +1,20 @@
+/**
+  * Nombre: Lizbeth Santillan 163721
+  * Fecha: 18/02/2019
+  * Hora: 6:43 p.m.
+*/
+
+import java.util.Scanner;
+
+class Ejercicio5 {
+
+public static void main (String[] args) {
+
+ Scanner in = new Scanner (System.in);
+ System.out.println("ingresar primer número:");
+ int num1 = in.nextInt();
+ System.out.println("ingresar segundo número:");
+ int num2 = in.nextInt();
+ System.out.println("el resultado es:" + (num1 * num2));
+}
+}
