b945cef30ef7f1a7dc1ea40832244d8b7ffa1476
163721Aneliz <46726319+163721aneliz@users.noreply.github.com>
2019-02-18 20:49:32 -0700

    Add files via upload

---
+++ Ejercicio10_SpecifiedFormula/Ejercicio10_SpecifiedFormula.java
@@ -0,0 +1,14 @@
+/**
+  * Nombre: Lizbeth Santillan 163721
+  * Fecha: 18/02/2019
+  * Hora: 8:41 p.m.
+*/
+
+class Ejercicio10 {
+ 
+public static void main(String[] args) {
+
+ System.out.println( 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11)));
+
+}
+}
