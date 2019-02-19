faabe9953c7524dd8210dcf69dfe681495ece136
163721Aneliz <46726319+163721aneliz@users.noreply.github.com>
2019-02-18 20:31:43 -0700

    Add files via upload

---
+++ Ejercicio9_SpecifiedExpressions/Ejercicio9_SpecifiedExpressions.java
@@ -0,0 +1,14 @@
+/** 
+  * Nombre: Lizbeth Santillan 163721
+  * Fecha: 18/02/2019
+  * Hora: 8:26 p.m.
+*/
+
+class Ejercicio9 {
+
+ public static void main(String[] arg) {
+
+  System.out.println((25.5 * 3.5 - 3.5 * 3.5) / (40.5-4.5));
+
+}
+}
