51c3f7b5e678c2b434b6b0459e495f9454376338
163721Aneliz <46726319+163721aneliz@users.noreply.github.com>
2019-02-18 20:21:52 -0700

    Add files via upload

---
+++ Ejercicio8_Pattern/Ejercicio8_Pattern.java
@@ -0,0 +1,17 @@
+/**
+  * Nombre: Lizbeth Santillan 163721
+  * Fecha: 18/02/2019
+  * Hora: 8:11 p.m.
+*/
+
+class Ejercicio8 {
+
+ public static void main(String[] args) {
+
+ System.out.println("   J    a    V     V  a ");
+ System.out.println("   J   a a    V   V  a a ");
+ System.out.println("J  J  aaaaa    V V  aaaaa ");
+ System.out.println(" JJ  a     a    V  a     a ");
+
+}
+}
