ed931a2e6e2cf157ca12189072d5fa514cba91d8
163721Aneliz <46726319+163721aneliz@users.noreply.github.com>
2019-02-18 20:10:40 -0700

    Add files via upload

---
+++ Ejercicio7_MultiplicationTable/Ejercicio7_MultiplicationTable.java
@@ -0,0 +1,23 @@
+/**
+  * Nombre: Lizbeth Santillan 163721
+  * Fecha: 18/02/2019
+  * Hora: 7:32 p.m.
+*/
+
+import java.util.Scanner;
+
+class Ejercicio7 {
+
+public static void main (String[] args) {
+
+ Scanner in = new Scanner (System.in);
+ System.out.println("Ingresar un número:");
+ int num = in.nextInt();
+ for (int i = 1; i <= 10; i++) {
+ System.out.println(num + "x" + i + "=" + (num * i));
+}
+}
+}
+ 
+ 
+
