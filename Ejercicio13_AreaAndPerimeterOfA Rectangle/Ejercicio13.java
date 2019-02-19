f0fadfaf5cb6c6f86c80acbb5d9c7c2f1a16d23a
163721Aneliz <46726319+163721aneliz@users.noreply.github.com>
2019-02-18 23:01:44 -0700

    Add files via upload

---
+++ Ejercicio13_AreaAndPerimeterOfA Rectangle/Ejercicio13.java
@@ -0,0 +1,25 @@
+/**
+  * Nombre: Lizbeth Santillan 163721
+  * Fecha: 18/02/2019
+  * Hora: 10:50 p.m.
+*/
+
+class Ejercicio13 {
+
+public static void main(String[] args) {
+
+ final double width = 5.6;
+ final double height = 8.5;
+ double perimeter, area;
+ 
+ perimeter = 2*(height + width);
+ area = width*height;
+
+System.out.println("El perimetro es:"+ perimeter);
+System.out.println("El area es:"+ area);
+
+}
+}
+ 
+  
+
